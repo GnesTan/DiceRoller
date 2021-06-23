3f1ce2eb5885728c46f9907dbdcf63a011800ac8
JiaSeng00 <jseng50@gmail.com>
2021-06-23 16:01:22 +0800

    Initial Commit v1.3

--- app/src/main/java/com/mad/diceroller/MainActivity.kt
+++ app/src/main/java/com/mad/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mad.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,13 +13,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
     }
+
     private fun rollDice(){
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
